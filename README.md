fa45c299201d3a839ce0daf8746c1caab371a9b8
iizuka <eo12w008@steng.u-hyogo.ac.jp>
2015-10-22 15:53:26 +0900

    fix mm89

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to run test
+
+```
+java MazeFixingVis -exec ./mm89 -seed 1 -debug -vis
+```
--- mm89.cpp
+++ mm89.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i,n) for(int i=0;i<(int)(n);++i)
+#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
 
 vector<string> improve(vector<string> maze, int F) {
   return vector<string>();
@@ -8,14 +8,18 @@ vector<string> improve(vector<string> maze, int F) {
 
 int main() {
   // height: [10, 80]
-  int H; cin >> H;
+  int H;
+  cin >> H;
   // width: [10, 80]
   vector<string> maze(H);
-  REP(i,H) getline(cin, maze[i]);
+  REP(i, H) getline(cin, maze[i]);
   // fixes: [N/10, N/3], N is the number of non '.' cells in the maze
-  int F; cin >> F;
+  int F;
+  cin >> F;
   vector<string> ret = improve(maze, F);
+  cout << ret.size() << endl;
   REP(i, ret.size()) {
     cout << ret[i] << endl;
   }
+  cout.flush();
 }
